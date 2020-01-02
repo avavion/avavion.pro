a77aa26899e6054ebfa9155ab614edbb05451064
avavion <avavionmvm@gmail.com>
2020-01-02 21:02:24 +0300

    Colors, images and more... Test build.

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,14 @@ const burgerButton = document.getElementById("burger-btn"),
 burgerButton.addEventListener("click", () => {
   menuList.classList.toggle("header-nav__menu-list--active");
 });
+
+let showMoreButton = document.querySelector("#btn-show-card");
+
+let workCards = document.querySelectorAll(".works-content-card");
+
+let currentShowCards = 2,
+  maxShowCards = currentShowCards + 2;
+
+showMoreButton.addEventListener("click", function() {
+  
+});
