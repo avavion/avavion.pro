7c7214e3a929c8fb165ca521187b5ce0d838dae7
avavion <avavionmvm@gmail.com>
2020-01-05 19:41:53 +0300

    Add skills. TEST build

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,8 @@
 // Burger button
 
-const burgerButton = document.getElementById("burger-btn"),
-  menuList = document.getElementById("menu-list");
+let burgerButton = document.getElementById("burger-btn");
+let menuList = document.getElementById("menu-list");
 
 burgerButton.addEventListener("click", () => {
   menuList.classList.toggle("header-nav__menu-list--active");
-});
-
-let showMoreButton = document.querySelector("#btn-show-card");
-
-let workCards = document.querySelectorAll(".works-content-card");
-
-let currentShowCards = 2,
-  maxShowCards = currentShowCards + 2;
-
-showMoreButton.addEventListener("click", function() {
-  
 });
